370984011c10e8acdece310a59ce797f3e44cff2
sanchezzzhak <sanchezzzhak@ya.ru>
2015-07-20 12:07:48 +0100

    first commit

---
+++ Area.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace kak\widgets\area;
+use yii\helpers\Html;
+
+class Area extends \yii\bootstrap\Widget
+{
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
+
+    /** @var array */
+    public $data = [];
+
+
+    public function init()
+    {
+
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        $areaId = $this->getAreaId();
+
+        echo Html::button('+' , ['class' => 'btn btn-info']);
+        echo Html::beginTag('script',[
+            'id' =>  $this->options['id'],
+            'type' => 'text/x-tmpl'
+        ]);
+        $this->getView()->beginBlock($areaId,true);
+
+    }
+
+    public function getAreaId()
+    {
+       return 'areaId' . $this->options['id'];
+    }
+
+
+    public function run()
+    {
+        $this->getView()->endBlock();
+        Html::endTag('script');
+        $view = $this->getView();
+        TmplAsset::register($view);
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+DubleBox widgets
+================
+DoubleBox widgets for Yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist dublebox/kak-dublebox "*"
+```
+
+or add
+
+```
+"dublebox/kak-dublebox": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \kak\widgets\AutoloadExample::widget(); ?>```
