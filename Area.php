c5f5de3044d166577fb3a115fe1b12a583a114ea
sanchezzzhak <sanchezzzhak@ya.ru>
2017-09-22 14:19:16 +0300

    adden support use view file template

--- Area.php
+++ Area.php
@@ -2,9 +2,29 @@
 
 namespace kak\widgets\area;
 
+use yii\base\Model;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
+/**
+ * use example
+ */
+/*
+```php
+
+
+
+
+
+
+```;
+ */
+
+
+/**
+ * Class Area
+ * @package kak\widgets\area
+ */
 class Area extends \yii\base\Widget
 {
     /**
@@ -12,17 +32,41 @@ class Area extends \yii\base\Widget
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
     public $options = [];
-    public $itemOptions = [];
+
+    /** @var string text button */
+    public $buttonLabel = '+';
+
+    /** @var array html options button add item */
     public $buttonOptions = [];
 
-    /** @var array */
-    public $label;
-    public $templateButtonAdding = '{button} {label}';
+    /** @var String|Closure render view file templated */
+    public $viewItem;
+
+    /** @var array addintion view params */
+    public $viewParams;
+
+    /** @var string template btn layout */
+    public $template = '{button} {label}';
+
+    /** @var array default item or model */
+    public $item;
+
+    /** @var array data array or models */
     public $items = [];
+
+    /** @var array html options block item */
+    public $itemOptions = [];
+
+    /** @var string text label */
+    public $label = 'add item';
+
+    /** @var array html options label */
+    public $labelOptions = [];
 
     public function init()
     {
         parent::init();
+
         $this->initDefaultOptions();
         $this->renderButtonAdd();
 
@@ -31,61 +75,116 @@ class Area extends \yii\base\Widget
             'type' => 'text/x-tmpl'
         ]);
         $this->getView()->beginBlock($this->getAreaId(), true);
+
         echo Html::beginTag('div', $this->itemOptions);
+
     }
 
+    /**
+     * @return string generate uuid block id
+     */
+    public function getAreaId()
+    {
+        return 'areaId' . $this->options['id'];
+    }
+
+    /**
+     * set default html options
+     */
     protected function initDefaultOptions()
     {
         if (!isset($this->options['id'])) {
             $this->options['id'] = $this->getId();
         }
         Html::addCssClass($this->itemOptions, 'areaItem');
+        Html::addCssClass($this->buttonOptions, 'btn');
     }
 
+    /**
+     * render html template placeholder
+     */
     protected function renderButtonAdd()
     {
-        Html::addCssClass($this->buttonOptions, 'btn');
         $this->buttonOptions = ArrayHelper::merge($this->buttonOptions, [
             'role' => 'area.add',
             'data-tmpl' => $this->options['id']
         ]);
-
-        $templateButtonAdding = strtr($this->templateButtonAdding, [
-            '{button}' => Html::button('+', $this->buttonOptions),
-            '{label}' => Html::tag('label', $this->label)
+        $template = strtr($this->template, [
+            '{button}' => Html::button($this->buttonLabel, $this->buttonOptions),
+            '{label}' => Html::label($this->label, $this->labelOptions)
         ]);
-        echo Html::tag('div', $templateButtonAdding, ['class' => 'form-group']);
+        echo Html::tag('div', $template, ['class' => 'form-group']);
     }
 
-    public function getAreaId()
+    /**
+     * render default item, view
+     * @return mixed|string
+     */
+    protected function renderTemplateItemForm()
     {
-        return 'areaId' . $this->options['id'];
+        if($this->viewItem===null){
+            return '';
+        }
+        return $this->renderTemplateItem($this->item);
     }
+
+    /**
+     * render store items, view
+     * @param $item
+     * @return mixed|string
+     */
+    protected function renderTemplateItem($item)
+    {
+        $params = $this->viewParams;
+        $params['item'] = $item;
+
+        if($this->viewItem instanceof \Closure){
+            return call_user_func($this->viewItem, $params, $this);
+        }
+        return $this->render($this->viewItem, $params);
+    }
+
+    /**
+     * render store items, block template
+     * @param $item
+     * @return mixed
+     */
+    protected function renderTemplateBlock($item)
+    {
+        $keys = $item instanceof Model ? $item->getAttributes() : array_keys($item);
+        $clip = (string)$this->getView()->blocks[$this->getAreaId()];
+        foreach ($keys as $key) {
+            $value = Html::encode($item[$key]);
+            $clip = str_replace('{%=o.' . $key . '%}', $value, $clip);
+        }
+        return $clip = preg_replace('#\{\%\=o(.*)\%\}#ism', '', $clip);
+    }
+
 
     public function run()
     {
+        echo $this->renderTemplateItemForm();
         echo Html::endTag('div');
+
         $this->getView()->endBlock();
+
         echo Html::endTag('script');
+
         echo Html::beginTag('div', [
             'id' => $this->getAreaId()
         ]);
         foreach ($this->items as $item) {
-            $keys = array_keys($item);
-            $clip = (string)$this->getView()->blocks[$this->getAreaId()];
-            foreach ($keys as $key) {
-                $value = Html::encode($item[$key]);
-                $clip = str_replace('{%=o.' . $key . '%}', $value, $clip);
+            echo Html::beginTag('div', $this->itemOptions);
+            if($this->viewItem !==null){
+                echo $this->renderTemplateItem($item);
+            }else{
+                echo $this->renderTemplateBlock($item);
             }
-            $clip = preg_replace('#\{\%\=o(.*)\%\}#ism', '', $clip);
-            echo $clip;
+            echo Html::endTag('div');
         }
-
         echo Html::endTag('div');
-        $view = $this->getView();
-        TmplAsset::register($view);
-        AreaAsset::register($view);
 
-
+        TmplAsset::register($this->getView());
+        AreaAsset::register($this->getView());
     }
 }
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ The widget can, Duplicate area by clicking the button
 Preview
 -----------
 <img src="https://lh3.googleusercontent.com/-G6BBKhyQqVg/Va0CmtW6v-I/AAAAAAAAACg/FnU_Qc9DyiU/s512-Ic42/areaPreview.png">
+
 Installation
 ------------
 The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
@@ -22,17 +23,17 @@ or add
 
 to the require section of your `composer.json` file.
 
-Usage
+Usage area widget
 -----
-Once the extension is installed, simply use it in your code by  :
+Once the extension is installed, simply use it in your code by:
 ```php
 <?php kak\widgets\area\Area::begin([
         'label' => 'add params',
         'items' => [
-            ['key' => 'key.name']
-            ['key' => 'key.name1']
-            ['key' => 'key.name2' , 'value' => '2']
-            ['key' => 'key.name3']
+            ['key' => 'key.name'],
+            ['key' => 'key.name1'],
+            ['key' => 'key.name2' , 'value' => '2'],
+            ['key' => 'key.name3'],
         ]
 ]);?>
     <div class="row form-group">
@@ -49,3 +50,56 @@ Once the extension is installed, simply use it in your code by  :
 <?php kak\widgets\area\Area::end();?>
 
 ```
+Usage area widget render template file
+-----
+Once the extension is installed, simply use it in your code by:
+```php
+<?php
+$models = PostParams::findOne($id);
+kak\widgets\area\Area::widget([
+    'item' => new PostParams,
+    'items' => $models,
+    'viewParams' => [
+        'form' => $form
+    ],
+    'viewItem' => '@app\views\post\_item-post-param'
+]);
+```
+viewItem is closure function support 
+```php
+<?php
+ kak\widgets\area\Area::widget([
+     'item' => new PostParams,
+     'items' => $models,
+     'viewParams' => [
+         'form' => $form
+     ],
+     'viewItem' => function($params, $thisArea){
+        
+     },
+ ]);   
+```
+
+## kak\widgets\area\Area
+Class Area
+@package kak\widgets\area
+
+*public* `buttonLabel` string text button
+
+*public* `buttonOptions` array html options button add item
+
+*public* `viewItem` String|Closure render view file templated
+
+*public* `viewParams` array addintion view params
+
+*public* `template` string template btn layout
+
+*public* `item` array default item or model
+
+*public* `items` array data array or models
+
+*public* `itemOptions` array html options block item
+
+*public* `label` string text label
+
+*public* `labelOptions` array html options label
